297962cd93be5f119164d26416cc5d9331fcfdfc
Patrick Pichler <git@patrickpichler.dev>
2023-11-13 15:06:36 +0100

    Initial commit

---
+++ components/Stack.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+import VClicks from '@slidev/client/builtin/VClicks.ts'
+
+</script>
+
+<style>
+.stack {
+  position: relative;
+}
+
+.stack > *{
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
+
+<template>
+  <div class="stack">
+    <VClicks>
+      <slot></slot>
+    </VClicks>
+  </div>
+</template>
---
+++ slides.md
@@ -0,0 +1,7 @@
+<Stack>
+
+# Hello
+
+# World
+
+</Stack>
